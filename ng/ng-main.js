aeda95d65dc0005c6021830d4ca28cd88e401c57
Mwero Abdalla <mwerothewebmaker@gmail.com>
2023-12-28 18:34:39 +0300

    chore : add config.php

--- ng/ng-main.js
+++ ng/ng-main.js
@@ -1,15 +1,14 @@
 var app = angular.module("myApp", []);
 
 const imageArray = [
-    "/img/kenindia.png",
-    "/img/ga-insurance.png",
-    "/img/mayfair-insurance.png",
-    "/img/intra.png",
-    "/img/geminia.png",
-    "/img/apa.png",
-  ];
+  "/img/kenindia.png",
+  "/img/ga-insurance.png",
+  "/img/mayfair-insurance.png",
+  "/img/intra.png",
+  "/img/geminia.png",
+  "/img/apa.png",
+];
 
-  
 const companyData = imageArray.map((src) => {
   // Extract image name from the src URL without the extension
   const imageName = src
@@ -28,6 +27,215 @@ const companyData = imageArray.map((src) => {
 // app.value("selectedCompany", "hello");
 app.service("companyService", function () {
   var selected = null;
+  var insuranceCategories = [
+    {
+      title: "Motor Insurance",
+      packages: [
+        {
+          title: "Comprehensive Insurance",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/motor_modal.html",
+          modalId: "motor",
+        },
+        {
+          title: "Third Party Insurance",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/motor_modal.html",
+          modalId: "motor",
+        },
+      ],
+    },
+    {
+      title: "Other Common Insurances",
+      packages: [
+        {
+          title: "Medical Insurance",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/health_modal.html",
+          modalId: "health",
+        },
+        {
+          title: "Travel Insurance",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/travel_modal.html",
+          modalId: "travel",
+        },
+        {
+          title: "Domestic Package",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/life_modal.html",
+          modalId: "life",
+        },
+        {
+          title: "Marine Insuranace",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/personal_modal.html",
+          modalId: "personal",
+        },
+      ],
+    },
+
+    {
+      title: "Business Insurance",
+      packages: [
+        {
+          title: "Burglary Insurance",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+
+        {
+          title: "Rental Property Insurance",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+        {
+          title: "Work Injury Benefits Act",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+        {
+          title: "Terrorism & Political Violence Liability",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+        {
+          title: "Stock Floater",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+        {
+          title: "Retirement Benefits Scheme",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+        {
+          title: "Public Liability",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+
+        {
+          title: "Machinery Breakdown",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+        {
+          title: "Industrial All Risks",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+        {
+          title: "Goods In Transit",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+        {
+          title: "Floriculture",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+        {
+          title: "Fire & Consequential Loss",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+        {
+          title: "Fire & Allied Perils",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+        {
+          title: "Fidelity Guarantee",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+        {
+          title: "Erection All Risks",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+        {
+          title: "Employers Liability",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+        {
+          title: "Electronic Equipment",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+
+        {
+          title: "Credit Insurance",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+        {
+          title: "Plant & Machinery",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+        {
+          title: "Contractors All Risk",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+        {
+          title: "Bonds",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+        {
+          title: "Boller and Pressure Vessel",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+        {
+          title: "Aviation",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+        {
+          title: "Agriculture",
+          image: "images/objects/6.png",
+          modalTemplate: "../ng/inc/general_modal.html",
+          modalId: "general",
+        },
+      ],
+    },
+  ];
+
+  this.setCategories = function (categories) {
+    insuranceCategories = categories;
+  };
+
+  this.getCategories = function () {
+    return insuranceCategories;
+  };
 
   this.setSelectedCompany = function (company) {
     selected = company;
@@ -38,7 +246,6 @@ app.service("companyService", function () {
   };
 });
 
-
 app.controller("vmaxController", function ($scope, companyService) {
   $scope.selectedCompany = companyService.getSelectedCompany();
   $scope.getSelectedCompany = () => companyService.getSelectedCompany();
@@ -46,6 +253,21 @@ app.controller("vmaxController", function ($scope, companyService) {
   $scope.menuToggle = function () {
     // Your menuToggle function logic here
   };
+
+  $scope.fetcheData = [];
+
+  getData();
+  async function getData() {
+    try {
+      const data = await (
+        await fetch("https://vmaxapi.fonfixrepairs.co.ke/insurances.php")
+      ).json();
+      $scope.fetchedData = data;
+      console.log({ data, fdata: $scope.fetcheddata });
+    } catch (error) {
+      console.log({ errorMessage: error.message });
+    }
+  }
 
   // Define your WhatsApp contacts
   $scope.whatsappContacts = [
@@ -107,17 +329,32 @@ app.controller("vmaxController", function ($scope, companyService) {
     },
   ];
 
+  // set insurance packages
+  function getCompanyData(company) {
+    let data =
+      $scope.fetcheData.filter((item) => company == item.company) ?? [];
+    return [
+      {
+        title: "Motor Insurance",
+        packages: data.filter((d) => d.category == "motor") ?? [],
+      },
+      {
+        title: "Other Common Insurances",
+        packages:
+          data.filter((d) => !["motor", "business"].includes(d.category)) ?? [],
+      },
+      {
+        title: "Business Insurance",
+        packages: data.filter((d) => d.category == "business") ?? [],
+      },
+    ];
+  }
   // Insurance
+  // $scope.insuranceCategories = getCompanyData("apa") ?? [
   $scope.insuranceCategories = [
     {
       title: "Motor Insurance",
       packages: [
-        // {
-        //   title: "Motor Insurance",
-        //   image: "images/objects/6.png",
-        //   modalTemplate: "../ng/inc/motor_modal.html",
-        //   modalId: "motor",
-        // },
         {
           title: "Comprehensive Insurance",
           image: "images/objects/6.png",
@@ -325,7 +562,6 @@ app.controller("vmaxController", function ($scope, companyService) {
       );
     }
   };
-
 });
 // app.controller("vmaxController", vmaxController);
 // components
@@ -346,6 +582,8 @@ app.component("companyLogos", {
 
       $scope.setSelectedCompany = (val) => {
         companyService.setSelectedCompany(val.trim());
+        companyService.setCategories()
+        $scope.insuranceCategories;
         console.log({ sel: $scope.getSelectedCompany() });
       };
     },
